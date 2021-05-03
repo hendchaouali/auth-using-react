922b28d9228166ae8d0343ddded601f822dcb0e8
hendchaouali <hendchaweli@gmail.com>
2021-05-03 17:07:33 +0200

    first commit

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,28 @@
-import {Route, BrowserRouter, Switch, Redirect} from "react-router-dom";
-import Login from "./components/Login/Login";
-import Dashboard from "./components/Dashboard/Dashboard";
-import NotFound from "./components/NotFound/NotFound";
+import React from "react";
 
+import {
+    Route,
+    BrowserRouter,
+    Switch,
+    Redirect,
+} from "react-router-dom";
+import Login from "./components/Login/Login";
+import NotFound from "./components/NotFound/NotFound";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+const authGuard = (Component) => () => {
+    return localStorage.getItem("token") ? (
+        <Component/>
+    ) : (
+        <Redirect to="/login"/>
+    );
+};
 export default function Routes() {
     return <BrowserRouter>
         <Switch>
             <Route exact path="/login" component={Login}>
             </Route>
-            <Route exact path="/dashboard" component={Dashboard}>
+            <Route exact path="/dashboard" render={authGuard(Dashboard)}>
             </Route>
             <Route exact path="/" component={Login}>
                 {/*<Redirect to="/login"/>*/}
@@ -19,3 +33,4 @@ export default function Routes() {
         </Switch>
     </BrowserRouter>
 }
+
